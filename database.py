9cf7dd2ba5476153f67ec36941985a75c7a96fec
hckcrow <65012369+hckcrow@users.noreply.github.com>
2022-10-22 17:40:58 +0700

    upload gui dasar

---
+++ database.py
@@ -0,0 +1,14 @@
+
+user1 = {
+    "name": "Zaki Yudhistira",
+    "age": "18",
+    "car": "SUV"
+};
+
+user2 = {
+    "name": "Bagas Sambega Rosyada",
+    "age": "18",
+    "car": "Terios"
+};
+
+
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from tkinter import *
+from datetime import date, datetime
+from PIL import ImageTk, Image
+
+waktu1 = datetime.now()
+waktuMasuk = waktu1.strftime("%H:%M:%S")
 
 windowUtama = Tk()
+width = windowUtama.winfo_screenwidth(); height = windowUtama.winfo_screenheight()
+windowUtama.geometry("%dx%d" % (width, height))
+windowUtama.title("Welcome to Jasamarga Tol")
+icon = Image.open('icon toll.png'); ico = ImageTk.PhotoImage(icon); windowUtama.wm_iconphoto(False, ico)
+windowUtama.configure(bg='#f0f4fa')
+
+
+# Label = Label(text= waktuMasuk)
+# Label.pack()
+
+
 windowUtama.mainloop()
