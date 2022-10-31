794cf8638b7dc1c5a89b549a5caf7b5d791467b4
Zaki <zakiyudhistira.itb69@gmail.com>
2022-10-31 21:12:35 +0700

    tambahan dikit

--- coba_ui_last.py
+++ coba_ui_last.py
@@ -6,4 +6,6 @@ width = int(dashboard.winfo_screenwidth()); height = int(dashboard.winfo_screenh
 Label(dashboard, text="Saldo Anda sisa : " + str(69000), font=("Montserrat", 15, 'bold')).place(x=width/2-95, y=800)
 Label(dashboard, text="Semoga Selamat Sampai Tujuan", font=("Montserrat", 15, 'bold')).place(x=width/2-135, y=800)
 end = Button(dashboard, text="Submit", font=("Lato", 15, 'bold'), padx=12, pady=3, state=NORMAL); end.place(x=width/2 - 20, y=height-200)
+Label(dashboard, text="Jarak : ", font=("Montserrat", 20, 'bold')).place(x=180, y=275)
+Label(dashboard, text=200, font=("Lato", 20, 'bold')).place(x=280, y=275)
 dashboard.mainloop()
